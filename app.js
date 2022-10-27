44bfb8ab1ff8373756d9d1b4d45bf9036db88021
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-10-27 02:59:20 +0000

    new:user route midleware

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//  signup and login auth
+require("./authentication/auth")
 
+const userRoute = require("./routes/authRoute");
 
+app.use("/api", userRoute);
 
+const { DbConnection } = require("./config/dbConnection");
+DbConnection();
 
-const {DbConnection} = require('./config/dbConnection')
-DbConnection()
+const PORT = process.env.PORT;
 
-
-const PORT = process.env.PORT
-
-app.listen(PORT, ()=>{
-    console.log(`APP IS RUNNING AT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`APP IS RUNNING AT ${PORT}`);
+});
