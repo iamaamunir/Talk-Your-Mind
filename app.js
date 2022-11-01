bc0f6348ce942f31e1a6fd12e74465349a7ee4c5
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-11-01 14:30:43 +0000

    added and create all routes

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+require("./authentication/auth")
 const express = require("express");
 const app = express();
+const passport = require('passport')
 require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//  signup and login auth
-require("./authentication/auth")
+app.use(express.json())
 
-const userRoute = require("./routes/authRoute");
+const userRouter = require("./routes/authRoute");
+const articleRouter = require('./routes/articleRoute')
+const publicRouter = require('./routes/publicRoute')
 
-app.use("/api", userRoute);
+
+app.use('/api', publicRouter)
+app.use("/api", userRouter);
+app.use('/api', passport.authenticate('jwt', { session: false }), articleRouter)
 
 const { DbConnection } = require("./config/dbConnection");
 DbConnection();
