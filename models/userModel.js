4932ba1059d9bfe16d735754daf89189b09b227f
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-11-04 18:03:59 +0000

    feat:ref articles field

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema
 const bcrypt = require('bcrypt')
+// 1. Users should have a first_name, last_name, email, password, (you can add other
+//     attributes you want to store about the user)
 const userSchema = new schema ({
     
     email:{
@@ -20,7 +22,13 @@ const userSchema = new schema ({
     last_name:{
         type:String,
         required:[true, 'last name is required']
+    },
+     article: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'articles'
     }
+]
 })
 
 userSchema.pre(
@@ -33,10 +41,20 @@ userSchema.pre(
     }
 )
 
+
 userSchema.methods.isValidPassword = async function(password){
     const user = this
     const compare = await bcrypt.compare(password, user.password)
     return compare
 }
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+    //   returnedObject.id = returnedObject._id.toString()
+    //   delete returnedObject._id
+      delete returnedObject.__v
+      
+    }
+  })
 
 module.exports = mongoose.model('user', userSchema)
