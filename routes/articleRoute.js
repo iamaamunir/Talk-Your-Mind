14fe9fee22cce5683596342e03628ba868037f43
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-11-04 22:54:14 +0000

    new:add all article routes

---
+++ routes/articleRoute.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const articleRouter = express.Router()
+
+const articleController = require('../controllers/articleController')
+
+
+articleRouter.post('/blog/create', articleController.createArticle)
+
+
+articleRouter.get('/blog/me/get',  articleController.getBlogByOwner)
+
+articleRouter.get('/blog/me/publish/:id', articleController.getPublishedArticle)
+articleRouter.patch('/blog/me/edit/blog/:id', articleController.updateById)
+articleRouter.patch('/blog/me/edit/state/:id', articleController.updateStateById)
+articleRouter.delete('/blog/me/delete/:id', articleController.deleteById)
+articleRouter.get('/blog/me/list' , articleController.getBlogList)
+
+module.exports = articleRouter
