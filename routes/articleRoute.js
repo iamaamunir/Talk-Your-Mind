c2704469472fed1b1ac22be65ab4cb05bcd63e0a
Munir Abdullahi <abdullahimunir3@gmail.com>
2022-11-06 10:45:06 +0000

    misc:refractor updateById url

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -10,7 +10,7 @@ articleRouter.post('/blog/create', articleController.createArticle)
 articleRouter.get('/blog/me/get',  articleController.getBlogByOwner)
 
 articleRouter.get('/blog/me/publish/:id', articleController.getPublishedArticle)
-articleRouter.patch('/blog/me/edit/blog/:id', articleController.updateById)
+articleRouter.patch('/blog/me/edit/:id', articleController.updateById)
 articleRouter.patch('/blog/me/edit/state/:id', articleController.updateStateById)
 articleRouter.delete('/blog/me/delete/:id', articleController.deleteById)
 articleRouter.get('/blog/me/list' , articleController.getBlogList)
